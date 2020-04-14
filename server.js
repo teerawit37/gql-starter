773cb33692c5106a1eaf84a2ecbdae7a8cee05cd
teerawit37 <t-rawit@hotmail.co.th>
2020-04-14 09:25:48 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var graphql = require('graphql');
+var GraphQLSchema = graphql.GraphQLSchema;
+var GraphQLObjectType = graphql.GraphQLObjectType;
+var GraphQLString = graphql.GraphQLString;
+var products = require('./data');
+var app = express();
+var PORT = process.env.port || 3000
+var queryType = new GraphQLObjectType({
+  name: "queryProduct",
+  description: "query of product",
+  fields: () => ({
+    hey: {
+      type: GraphQLString,
+      resolve: function(_, args){
+        return products
+      }
+    }
+  })
+});
+var MyGraphQLSchema = new GraphQLSchema({
+  query: queryType
+});
+app.use('/graphql', graphqlHTTP({
+  schema: MyGraphQLSchema,
+  graphiql: true
+}));
+app.listen(PORT);
+console.log("Server running on localhost:", PORT);
